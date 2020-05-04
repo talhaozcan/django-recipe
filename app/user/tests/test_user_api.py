a05f3fc0ce1a3caf3995d576682c36f91cee9a03
Ahmet Talha Ozcan <a.talhaozcan@gmail.com>
2020-05-04 18:52:48 -0400

    added manage user end point

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -8,6 +8,7 @@ from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
+ME_URL = reverse('user:me')
 
 
 def create_user(**params):
@@ -120,3 +121,52 @@ class PublicUserAPITests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertNotIn('token', res.data)
+
+    def test_retreive_profile_unauthorized(self):
+        """Test that auth is required"""
+        res = self.client.get(ME_URL)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateUserAPITests(TestCase):
+    """Tests private(auth) user apis"""
+
+    def setUp(self):
+        params = {
+            'email': 'test@email.com',
+            'password': 'demo1234',
+            'name': 'Test User'
+        }
+        self.user = create_user(**params)
+
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_retreive_profile(self):
+        """Test retreiving profile successfully"""
+        res = self.client.get(ME_URL)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+        self.assertEqual(
+            res.data,
+            {'email': self.user.email, 'name': self.user.name}
+        )
+
+    def test_post_me_not_allowed(self):
+        """Test that post to me is not allowed"""
+
+        res = self.client.post(ME_URL, {})
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_profile(self):
+
+        params = {
+            'name': 'Updating Name',
+            'password': 'updatingpassword'
+        }
+        res = self.client.patch(ME_URL, params)
+        self.user.refresh_from_db()
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.user.name, params['name'])
+        self.assertTrue(self.user.check_password(params['password']))
